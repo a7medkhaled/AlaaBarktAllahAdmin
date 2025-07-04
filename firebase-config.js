2e216dba16452346b4ef64fb792f4846ce73b802
ahmed <ahmedkhaled3000@gmail.com>
2025-07-04 19:25:44 +0300

    adding dev environment

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
+const response = await fetch("./firebaseKeys.json");
+const firebaseConfig = await response.json();
+console.log(firebaseConfig);
+
 // Replace the below config with your actual Firebase project config
-const firebaseConfig = {
-  apiKey: "AIzaSyBiuIS2-tmFhIsMyzpk6W-PvU5os0ukfVs",
-  authDomain: "alaabarktallah.firebaseapp.com",
-  projectId: "alaabarktallah",
-  storageBucket: "alaabarktallah.firebasestorage.app",
-  messagingSenderId: "893572008389",
-  appId: "1:893572008389:web:51c2b9586e8073c83223ac",
-  measurementId: "G-1WQCKRXCYB",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBiuIS2-tmFhIsMyzpk6W-PvU5os0ukfVs",
+//   authDomain: "alaabarktallah.firebaseapp.com",
+//   projectId: "alaabarktallah",
+//   storageBucket: "alaabarktallah.firebasestorage.app",
+//   messagingSenderId: "893572008389",
+//   appId: "1:893572008389:web:51c2b9586e8073c83223ac",
+//   measurementId: "G-1WQCKRXCYB",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
